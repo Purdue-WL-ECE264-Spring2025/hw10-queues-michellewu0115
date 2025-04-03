5402c28d3b9e96a7b06eda9adf098c6f8ce9b7de
michellewu0115 <wu1867@purdue.edu>
2025-04-03 17:50:57 -0400

    change

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,66 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  struct list_node *node = malloc(sizeof(struct list_node));
+  if (!node) return NULL;
+  node->value = value;
+  node->next = NULL;
+  return node;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *node = new_node(value);
+  if (!node) return;
+  node->next = list->head;
+  list->head = node;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *node = new_node(value);
+  if (!node) return;
+  if (!list->head) {
+    list->head = node;
+  } else {
+    struct list_node *cur = list->head;
+    while (cur->next) cur = cur->next;
+    cur->next = node;
+  }
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  if (!list->head) return 0;
+  struct list_node *temp = list->head;
+  size_t value = temp->value;
+  list->head = temp->next;
+  free(temp);
+  return value;
+ }
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) {
+  if (!list->head) return 0;
+  if (!list->head->next) {
+    size_t value = list->head->value;
+    free(list->head);
+    list->head = NULL;
+    return value;
+  }
+  struct list_node *cur = list->head;
+  while (cur->next->next) cur = cur->next;
+  size_t value = cur->next->value;
+  free(cur->next);
+  cur->next = NULL;
+  return value;
+ }
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) {
+  struct list_node *cur = list.head;
+  while (cur) {
+    struct list_node *temp = cur;
+    cur = cur->next;
+    free(temp);
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,75 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    insert_at_tail(&q->data, serialize(state));
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) {
+    return deserialize(remove_from_head(&q->data));
+    }
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) { 
+    struct queue q = {0};
+  enqueue(&q, start);
+
+  // Create visited hash table of 2^24 entries (just enough for unique states)
+  // This is a simple approach. Better: use a hash map.
+  const size_t SIZE = 1 << 24;
+  bool *visited = calloc(SIZE, sizeof(bool));
+  if (!visited) return -1;
+
+  while (q.data.head) {
+    struct game_state state = dequeue(&q);
+    uint64_t s = serialize(state);
+    size_t index = s % SIZE;
+
+    if (visited[index]) continue;
+    visited[index] = true;
+
+    // Check if solved
+    bool solved = true;
+    uint8_t expected = 1;
+    for (uint8_t i = 0; i < 4; i++) {
+      for (uint8_t j = 0; j < 4; j++) {
+        if (i == 3 && j == 3) {
+          if (state.tiles[i][j] != 0) solved = false;
+        } else if (state.tiles[i][j] != expected++) {
+          solved = false;
+        }
+      }
+    }
+    if (solved) {
+      free(visited);
+      free_list(q.data);
+      return state.num_steps;
+    }
+
+    // Try all moves
+    struct game_state next;
+    if (state.empty_row < 3) {
+      next = state;
+      move_up(&next);
+      enqueue(&q, next);
+    }
+    if (state.empty_row > 0) {
+      next = state;
+      move_down(&next);
+      enqueue(&q, next);
+    }
+    if (state.empty_col < 3) {
+      next = state;
+      move_left(&next);
+      enqueue(&q, next);
+    }
+    if (state.empty_col > 0) {
+      next = state;
+      move_right(&next);
+      enqueue(&q, next);
+    }
+  }
+
+  free(visited);
+  free_list(q.data);
+  return -1;
+ }
