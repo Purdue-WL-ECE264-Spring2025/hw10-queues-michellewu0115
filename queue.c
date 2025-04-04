6cee268ac4e7314d17e16621bb8b81fce17a552c
michellewu0115 <wu1867@purdue.edu>
2025-04-04 15:40:59 -0400

    c

--- queue.c
+++ queue.c
@@ -24,8 +24,7 @@ int number_of_moves(struct game_state start) {
 
   while (q.data.head) {
     struct game_state state = dequeue(&q);
-    uint64_t s = serialize(state);                      
-    size_t index = (s ^ (s >> 32)) % SIZE;              
+    size_t index = (serialize(state) ^ (serialize(state) >> 32)) % SIZE;              
 
     if (visited[index]) continue;
     visited[index] = true;
